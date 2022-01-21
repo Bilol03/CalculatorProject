e6994fce6041913317db197a24e2c3311f2d8bac
Bilol <madiyarovbilol78@gmail.com>
2022-01-21 23:41:22 +0500

    The project has been fully finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let nums = document.querySelectorAll('.num')
 let signs = document.querySelectorAll('.sign')
-let clearElements = document.querySelector('.remove-element')[0]
-let removeElements = document.querySelector('.remove-element')[1]
+let clearElements = document.querySelectorAll('.remove-element')[0]
+let removeElements = document.querySelectorAll('.remove-element')[1]
 let dot = document.querySelector('.dot')
 let equal = document.querySelector('.equal')
 let input = document.querySelector('#display')
@@ -27,9 +27,69 @@ class Calculator {
 	}
 
 	sign(value) {
+		if(
+			!display.value ||
+			( 	
+				(typeof +display.value[display.value.length - 1]) === 'number' && 
+				!isNaN(+display.value[display.value.length - 1]) &&
+				this.operator 
+			) || 
+			display.value[display.value.length - 1] === '.'
+		) return
 
+		display.value = this.operator ? display.value.slice(0, display.value.length - 1) + value :  display.value + value
+		this.operator = value
+		this.isDot = false
 	}
 
+	dot(value) {
+		if(
+			this.isDot ||
+			!display.value || 
+			display.value[display.value.length - 1] === '.' ||
+			this.operators.includes(display.value[display.value.length - 1])
+		) return
+	
+		display.value = display.value + value
+		this.isDot = true
+	}
+
+	calculate() {
+		if(this.operators.includes(display.value[display.value.length - 1])) return
+		const [ num1, num2 ] = display.value.split(this.operator)
+		switch(this.operator) {
+			case '+': {
+				display.value = num1 + num2
+				break
+			}
+			case '-': {
+				display.value = num1 - num2
+				break
+			}
+			case '✕': {
+				display.value = num1 * num2
+				break
+			}
+			case '÷': {
+				display.value = num1 / num2
+				break
+			}
+		}
+		this.operator = null
+		if(display.value.includes('.')) this.isDot = true
+	}
+
+	clear() {
+		display.value = null
+		this.operator = null
+		this.isDot = false
+	}
+
+	remove () {
+		if(display.value[display.value.length - 1] === '.') this.isDot = false
+		if(this.operators.includes(display.value[display.value.length - 1])) this.operator = null
+		display.value = display.value.slice(0, display.value.length - 1)
+	}
 
 
 }
@@ -48,6 +108,13 @@ for(let sign of signs) {
 		calc.sign(sign.textContent)
 	}
 }
+
+dot.onclick = () => calc.dot(dot.textContent)
+equal.onclick = () => calc.calculate()
+clearElements.onclick = () => calc.clear()
+removeElements.onclick = () => calc.remove()
+
+
 
 // numbers.addEventListener('click', calculator.numbers())
 
